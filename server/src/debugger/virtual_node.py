d4f007c1fcfa8d3c6170041b1caae739bd400bc1
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-03-10 21:50:47 +0100

    Update virtual_node.py

--- server/src/debugger/virtual_node.py
+++ server/src/debugger/virtual_node.py
@@ -12,26 +12,17 @@ class VirtualNode:
         breakpoint: Optional[bool] = False,
     ):
         self.original_name = name
-        self._name = f"VIRTUALNODE_{name}"
+        self.name = name
         self.func = func
         self.next = next_node
         self.breakpoint = breakpoint
 
-    @property
-    def name(self) -> str:
-        if self._name == f"VIRTUALNODE_{START}":
-            return START
-        elif self._name == f"VIRTUALNODE_{END}":
-            return END
-        else:
-            return self._name
-
     def build(self, builder: StateGraph):
-        builder.add_node(self._name, self.func)
+        builder.add_node(self.name, self)
 
     def __repr__(self) -> str:
-        return f'VirtualNode(name="{self._name}", func={self.func})'
+        return f'VirtualNode(name="{self.name}", func={self.func})'
 
     def __call__(self, *args, **kwds):
-        print(f"Calling {self._name}")
+        print(f"Calling {self.name}")
         return self.func(*args, **kwds)
